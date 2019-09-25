1fb5e5a605d56341966d3d0807c29d99bcc97973
gabrielpda15 <gabriel.pda15@gmail.com>
2019-09-25 11:16:24 -0300

    Inclusão da base de location

---
+++ KuuhakuFramework.Web.Models/Models/Location/Country.cs
@@ -0,0 +1,29 @@
+﻿using KuuhakuFramework.Web.Models.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuuhakuFramework.Web.Models.Location
+{
+    [Table("Location_Country")]
+    public class Country : BaseEntity
+    {
+        [StringLength(120)]
+        [DataType("varchar")]
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public string Name { get; set; }
+
+        [StringLength(2)]
+        [DataType("varchar")]
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "Código é obrigatório")]
+        public string Code { get; set; }
+
+        public virtual IList<Region> Regions { get; set; }
+    }
+}
---
+++ KuuhakuFramework.Web.Models/Models/Location/Region.cs
@@ -0,0 +1,25 @@
+﻿using KuuhakuFramework.Web.Models.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuuhakuFramework.Web.Models.Location
+{
+    [Table("Location_Region")]
+    public class Region : BaseEntity
+    {        
+        [StringLength(120)]
+        [DataType("varchar")]
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [ScaffoldColumn(false)]
+        public string Name { get; set; }
+        public virtual int CountryId { get; set; }
+
+        public virtual IList<City> Cities { get; set; }
+    }
+}
