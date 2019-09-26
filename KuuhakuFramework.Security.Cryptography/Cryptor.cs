6752484c6ef9ea886f2b41a873c035d00e81e17d
gabrielpda15 <gabriel.pda15@gmail.com>
2019-09-26 14:46:36 -0300

    Criação da parte de criptografia

---
+++ KuuhakuFramework.Security.Cryptography/Cryptor.cs
@@ -0,0 +1,51 @@
+﻿using KuuhakuFramework.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuuhakuFramework.Security.Cryptography
+{
+    public static class Cryptor
+    {
+        public static byte[] EncryptBytes(byte[] input, byte[] phrase)
+        {
+            using (var TDESAlgorithm = new TripleDESCryptoServiceProvider())
+            {
+                TDESAlgorithm.Key = phrase;
+                TDESAlgorithm.Mode = CipherMode.ECB;
+                TDESAlgorithm.Padding = PaddingMode.PKCS7;
+                using (var cryptor = TDESAlgorithm.CreateEncryptor())
+                {
+                    return cryptor.TransformFinalBlock(input, 0, input.Length);
+                }
+            }
+        }
+
+        public static byte[] DecryptBytes(byte[] input, byte[] phrase)
+        {
+            using (var TDESAlgorithm = new TripleDESCryptoServiceProvider())
+            {
+                TDESAlgorithm.Key = phrase;
+                TDESAlgorithm.Mode = CipherMode.ECB;
+                TDESAlgorithm.Padding = PaddingMode.PKCS7;
+                using (var cryptor = TDESAlgorithm.CreateDecryptor())
+                {
+                    return cryptor.TransformFinalBlock(input, 0, input.Length);
+                }
+            }
+        }
+
+        public static string Encrypt(string message, string phrase)
+        {
+            return EncryptBytes(message.FromUTF8(), Phraser.GenerateHash(phrase)).ToBase64();
+        }
+
+        public static string Decrypt(string message, string phrase)
+        {
+            return DecryptBytes(message.FromBase64(), Phraser.GenerateHash(phrase)).ToUTF8();
+        }
+    }
+}
---
+++ KuuhakuFramework.Security.Cryptography/Phraser.cs
@@ -0,0 +1,45 @@
+﻿using KuuhakuFramework.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuuhakuFramework.Security.Cryptography
+{
+    public static class Phraser
+    {
+        public static byte[] GenerateBytePhrase(int length = 16, string chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmnnOoPpQqRrSsTtUuVvWwXxYyZz0123456789")
+        {
+            var phrase = GeneratePhrase(length, chars);
+            return GenerateHash(phrase);
+        }
+
+        public static string GeneratePhrase(int length = 16, string chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmnnOoPpQqRrSsTtUuVvWwXxYyZz0123456789")
+        {
+            var rnd = new Random(new Random().Next());
+            var temp = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                temp += chars[rnd.Next(chars.Length)].ToString();
+            }
+
+            return temp;
+        }
+
+        public static byte[] GenerateHash(byte[] value)
+        {
+            using (var hashProvider = new MD5CryptoServiceProvider())
+            {
+                return hashProvider.ComputeHash(value);
+            }
+        }
+
+        public static byte[] GenerateHash(string value)
+        {
+            return GenerateHash(value.FromUTF8());
+        }
+    }
+}
