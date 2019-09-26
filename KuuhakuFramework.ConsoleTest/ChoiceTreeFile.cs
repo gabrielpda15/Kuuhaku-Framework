2a65b91bad5fe23582a99d323dd1cd0388c2e0e3
gabrielpda15 <gabriel.pda15@gmail.com>
2019-09-26 16:09:38 -0300

    Projeto de testes

---
+++ KuuhakuFramework.ConsoleTest/ChoiceTreeFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuuhakuFramework.ConsoleTest
+{
+    public class ChoiceTreeFile : ChoiceTree.Branched.IO.ChoiceTreeFile
+    {
+        protected override char[] Magic => new char[] { 'C', 'T', 'F' };
+
+        protected override string Version => "1.0.0";
+
+        protected override int PhraseLength => 16;
+
+        public ChoiceTreeFile() : base() { }
+        public ChoiceTreeFile(ChoiceTree.Branched.ChoiceTree choiceTree) : base(choiceTree) { }
+    }
+}
---
+++ KuuhakuFramework.ConsoleTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using KuuhakuFramework.ChoiceTree.Branched.Facts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuuhakuFramework.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var choiceTree = new ChoiceTree.Branched.ChoiceTree("Teste", "Um testiculo muito fofo");
+
+            var trab = choiceTree.AddEvent(new Event("Burguesia, Burguesia, Burguesiaaaaaaaaa").ToEnd());
+            var vagaba = choiceTree.AddEvent(new Event("Bora joga lol então!").ToEnd());
+
+            var choice1 = choiceTree.AddChoice(new Choice("Você é o que?")
+                            .AddAnswer("Estudante").ToEnd()
+                            .AddAnswer("Trabalhador Brasileiro").ToEvent(trab)
+                            .AddAnswer("Vagabundo").ToEvent(vagaba));
+                
+            choiceTree.EntryPoint = Reaction.CreateVarChoiceReaction(choiceTree.AddVarChoice(new VarChoice("Qual o seu nome?", "name").ToChoice(choice1)));
+
+            var file = new ChoiceTreeFile(choiceTree);
+
+            file.WriteBytes("temp.dat");*/
+
+            var file = new ChoiceTreeFile();
+            file.ReadBytes("temp.dat");
+        }
+    }
+}
